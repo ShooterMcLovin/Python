fc44a9e73efdba1af1271c3fff1aacc69a0acfd2
al <you@example.com>
2024-12-05 09:06:57 -0500

    Auto-Commit

---
+++ ex09/ft_package/README.md
@@ -0,0 +1,14 @@
+A simple package that counts the occurrences of an item in a list.
+
+## Installation
+
+You can install the package from a local directory by running:
+
+```bash
+pip install ./dist/ft_package-0.0.1.tar.gz
+```
+## Running
+From the base directory:
+```bash
+python3 tester.py
+```
---
+++ ex09/ft_package/ft_package/count_in_list.py
@@ -0,0 +1,12 @@
+def count_in_list(lst, item):
+    """
+    Count the occurrences of an item in a list.
+
+    Parameters:
+    lst (list): The list in which to count occurrences.
+    item: The item to count in the list.
+
+    Returns:
+    int: The number of times the item appears in the list.
+    """
+    return lst.count(item)
