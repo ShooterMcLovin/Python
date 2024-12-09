1af1356e192a6584287715aec5681c4680b7a2dc
al <you@example.com>
2024-12-09 13:24:46 -0500

    Auto-Commit

--- Python1/ex03/load_image.py
+++ Python1/ex03/load_image.py
@@ -24,3 +24,32 @@ def ft_load(path: str) -> np.ndarray:
         print(f"Error: The file at path '{path}' is not a valid image or is unsupported.")
     except Exception as e:
         print(f"Error: {e}")
+
+def slice_me(family: list, start: int, end: int) -> list:
+    """
+    Slices a list based on start and end
+    """
+    if family.__class__ != list:
+        raise ValueError("The input should be a list.")
+
+    row_length = len(family[0]) if family else 0
+    for row in family:
+        if len(row) != row_length:
+            raise ValueError("All rows must be of the same length.")
+
+    shape = (len(family), len(family[0]))
+    print(f"My shape is : {shape}")
+
+    try:
+        sliced_array = family[start:end]
+    except IndexError:
+        raise ValueError("Invalid start or end index.")
+
+    if sliced_array:
+        new_shape = (len(sliced_array), len(sliced_array[0]))
+    else:
+        new_shape = (0, 0)
+
+    print(f"My new shape is : {new_shape}")
+
+    return sliced_array
